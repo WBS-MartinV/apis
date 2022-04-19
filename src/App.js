64e21239e1dee70262766ab19ac9ca32b4d9a058
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-04-19 14:20:22 +0200

    Copy everything over

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>My Amazing Users</h1>
+
+            <div className="block">
+                In this exercise you will have to fetch the data from an API
+                <br />
+                <br />
+                Look at the instructions in <strong>index.js</strong>
+                <br />
+                <a
+                    className="link"
+                    target="_blank"
+                    href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch"
+                >
+                    Help
+                </a>
+            </div>
+
+            <div className="block">
+                <ul id="users"></ul>
+            </div>
+        </div>
+    );
 }
+
+const ul = document.getElementById("users"); // Get the element where we will place our users
+const url = "https://randomuser.me/api/?results=15&&gender=female";
+//https://randomuser.me/api/?results=50"; // Get 10 random users
+
+const renderUsers = (users) => {
+  console.log(users);
+  return users.map((user) => {
+    console.log(user);
+    return (ul.innerHTML += `
+    <li>
+      <img src="${user.picture.medium}" />
+      <p>${user.name.first} ${user.name.last}</p>
+      <div>${user.email}</div>
+      <div>${user.phone}</div>
+    </li>`);
+  });
+};
+
+/**
+ * Don't edit the code above
+ * ---
+ * Below you will have to fetch the data from the link (stored in the variable url)
+ * Once retrieved, you will pass the result as an argument of the function renderUsers
+ */
+
+// This takes the raw response from the server and returns just the json.
+const responseHandler = (response) => {
+  console.debug(response);
+  return response.json();
+};
+
+fetch(url)
+  .then(responseHandler)
+  .then((json) => renderUsers(json.results));
 
 export default App;
