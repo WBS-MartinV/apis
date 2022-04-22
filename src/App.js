12343d1ac8c3a36b6c19e885bbe2dbaf48e9b7de
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-04-22 16:07:35 +0200

    Add sorting, titles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function User(user) {
         <li key={user.phone}>
             <img src={user.picture.medium} />
             <p>
-                {user.name.first} {user.name.last}
+                {user.name.first} {user.name.last} ({user.registered.age})
             </p>
             <div>{user.email}</div>
             <div>{user.phone}</div>
@@ -15,46 +15,91 @@ function User(user) {
 }
 
 function App() {
-    const [users, setUsers] = useState(false);
-    // const [query, setQuery] = useState("?results=15&gender=female");
-    const [number, setNumber] = useState(10);
-    const [gender, setGender] = useState(undefined);
-
     const url = "https://randomuser.me/api/";
+
+    const [users, setUsers] = useState(false);
+    const [number, setNumber] = useState(10);
+    const [gender, setGender] = useState();
+
+    const [sort, setSort] = useState();
+
+    const byRegistered = (a, b) => {
+        return new Date(a.registered.date) - new Date(b.registered.date);
+    };
+
+    const fullName = (user) => [user.name.first, user.name.last].join(" ");
+    const byName = (a, b) => fullName(a).localeCompare(fullName(b));
 
     useEffect(() => {
         fetch(url + `?results=${number}&gender=${gender}`)
             .then((response) => response.json())
-            .then((json) => setUsers(json.results));
+            .then((json) => {
+                console.log("Fetched ", json);
+                setUsers(json.results);
+            });
     }, [number, gender]);
 
     return (
         <div className="App">
             <div className="block">
-                <input
-                    type="range"
-                    value={number}
-                    onChange={(e) => setNumber(e.target.value)}
-                />{" "}
-                {number}
-                <button
-                    style={{ color: gender === "male" ? "red" : "black" }}
-                    onClick={() => setGender(gender === "male" ? "" : "male")}
-                >
-                    Male
-                </button>
-                <button
-                    style={{ color: gender === "female" ? "red" : "black" }}
-                    onClick={() =>
-                        setGender(gender === "female" ? "" : "female")
-                    }
-                >
-                    Female
-                </button>
-                <button onClick={() => setUsers(users.slice(1))}>
-                    Remove first
-                </button>
-                <ul id="users">{users && users.map(User)}</ul>
+                <div>
+                    <h3>API:</h3>
+                    <input
+                        type="range"
+                        value={number}
+                        onChange={(e) => setNumber(e.target.value)}
+                    />
+                    {number}
+
+                    <button
+                        style={{ color: gender === "male" ? "red" : "black" }}
+                        onClick={() =>
+                            setGender(gender === "male" ? "" : "male")
+                        }
+                    >
+                        Male
+                    </button>
+                    <button
+                        style={{ color: gender === "female" ? "red" : "black" }}
+                        onClick={() =>
+                            setGender(gender === "female" ? "" : "female")
+                        }
+                    >
+                        Female
+                    </button>
+                </div>
+
+                <div>
+                    <h3>Sorting:</h3>
+                    <button
+                        style={{
+                            color: sort?.name === byName.name ? "red" : "black",
+                        }}
+                        onClick={() => setSort(() => byName)}
+                    >
+                        Sort by name
+                    </button>
+                    <button
+                        style={{
+                            color:
+                                sort?.name === byRegistered.name
+                                    ? "red"
+                                    : "black",
+                        }}
+                        onClick={() => setSort(() => byRegistered)}
+                    >
+                        Sort by registered
+                    </button>
+                </div>
+
+                <div>
+                    <h3>Array: </h3>
+                    <button onClick={() => setUsers(users.slice(1))}>
+                        Remove first
+                    </button>
+                    <button onClick={() => setUsers([])}>Clear</button>
+                    <ul id="users">{users && users.sort(sort).map(User)}</ul>
+                </div>
             </div>
         </div>
     );
